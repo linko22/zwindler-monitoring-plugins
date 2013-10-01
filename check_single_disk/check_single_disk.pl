09a1bb94f8cbfe7830a17b0e5abc42fbcc47ad74
zwindler <dt.germain@gmail.com>
2013-10-01 11:51:38 +0200

    Update check_single_disk.pl

--- check_single_disk/check_single_disk.pl
+++ check_single_disk/check_single_disk.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
-
+############################################################################
+#This file is a part of zwindler-monitoring-plugins repository
+#Copyright (C) 2013 zwindler
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see {http://www.gnu.org/licenses/}.
 ################################################################################
 # Simplification of check_all_disks.pl (YM?) to check only one disk
 # check_single_disk.pl [FS_to_check] [warn] [crit]
