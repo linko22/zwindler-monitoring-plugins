f47f4553e5b78e7215c64872c6a649604b61dcc4
zwindler <dt.germain@gmail.com>
2013-10-02 14:35:07 +0200

    Update check_iostat.pl

--- check_iostat/check_iostat.pl
+++ check_iostat/check_iostat.pl
@@ -29,6 +29,7 @@ my %ERRORS = ('OK', '0',
                 'CRITICAL', '2',
                 'UNKNOWN', '3');
 
+#my $selected_disk = shift || &usage(%ERRORS);
 my $warn = shift || 100*1024*1024;
 my $critical = shift || 200*1024*1024;
 my $ignore_error_output = "2>/dev/null";
@@ -41,51 +42,51 @@ exit $ERRORS{"$state"};
 ### Subroutines ##############################################
 
 sub iostat_warnings
-        {
-        my @t = @_;
-        my $state = "OK";
-        my $perfdata;
-        my $print_answer;
+	{
+	my @t = @_;
+	my $state = "OK";
+	my $perfdata;
+	my $print_answer;
 
-        #Disks information
-        my $disk;
-        my $disk_name;
-        my $disk_rate;
-        my $vg_name;
-        my $vg_table = {};
+	#Disks information
+	my $disk;
+	my $disk_name;
+	my $disk_rate;
+	my $vg_name;
+	my $vg_table = {};
 
-        #Here we sort singles disks from disks in VG
-        foreach $disk (@t)
-                {
-                ($disk_name,$disk_rate,$vg_name) = split(";", $disk);
-                if ( $vg_name eq "NOVG" )
-                        {
-                        #Single disk
-                        $perfdata .= $disk_name."=".$disk_rate."Bps;$warn;$critical;0; ";
-                        }
-                else
-                        {
-                        #PV disk
-                        $vg_table{$vg_name} = $vg_table{$vg_name} + $disk_rate;
-                        #DEBUG
-                        #print $vg_name." ".$vg_table{$vg_name}."\n";
-                        }
-                }
+	#Here we sort singles disks from disks in VG
+	foreach $disk (@t)
+		{
+		($disk_name,$disk_rate,$vg_name) = split(";", $disk);
+		if ( $vg_name eq "NOVG" )
+			{
+			#Single disk
+			$perfdata .= $disk_name."=".$disk_rate."Bps;$warn;$critical;0; ";
+			}
+		else
+			{
+			#PV disk
+			$vg_table{$vg_name} = $vg_table{$vg_name} + $disk_rate;
+			#DEBUG
+			#print $vg_name." ".$vg_table{$vg_name}."\n";
+			}
+		}
 
-        #Now we have the total Bps for VGs from PVs, we can print it
-        for my $key ( keys %vg_table )
-                {
-                my $value = $vg_table{$key};
-                $perfdata .= $key."=".$value."Bps;$warn;$critical;0; ";
-                }
+	#Now we have the total Bps for VGs from PVs, we can print it
+	for my $key ( keys %vg_table ) 
+		{
+		my $value = $vg_table{$key};
+		$perfdata .= $key."=".$value."Bps;$warn;$critical;0; ";
+		}
 
-        #TODO : Add threshold
-        #print "OK: $print_answer, below warning/critical thresholds| $perfdata\n";
-        #$perfdata =~ s/\./,/;
-        print "OK | $perfdata\n";
+	#TODO : Add threshold
+	#print "OK: $print_answer, below warning/critical thresholds| $perfdata\n";
+	#$perfdata =~ s/\./,/;
+	print "OK | $perfdata\n";
 
-        return($state);
-        }
+	return($state);
+	}
 
 sub os_uname
         {
@@ -96,150 +97,170 @@ sub os_uname
         }
 
 sub getpvpervg_linux
-        {
+	{
+	my ($vg_cmd) = @_;
+	my $pvtable = {};
+	$current_vg;
+	$current_pv;
 
-        }
+	open(VGDISPLAYOUT,"$vg_cmd $ignore_error_output |") || die;
+	$_ = <VGDISPLAYOUT>;
+	while($_ = <VGDISPLAYOUT>)
+		{
+		#DEBUG
+		#print $_;
+		if (/\/dev\/([\w-]+\d*)\s+(\S+)\s+/)
+			{
+			$current_pv=$1;
+			$current_vg=$2;
+			#DEBUG
+			#print "$current_pv $current_vg\n";
+			$pvtable{$current_pv} = $current_vg;
+			}
+		}
+	return $pvtable;
+	}
 
 sub getpvpervg_hpux
-        {
-        my ($vg_cmd) = @_;
-        my $pvtable = {};
-        $current_vg;
-        $current_pv;
+	{
+	my ($vg_cmd) = @_;
+	my $pvtable = {};
+	$current_vg;
+	$current_pv;
 
-        open(VGDISPLAYOUT,"$vg_cmd $ignore_error_output |") || die;
-        $_ = <VGDISPLAYOUT>;
-        while($_ = <VGDISPLAYOUT>)
-                {
-                if (/^vg_name=\/dev\/(\w+\d+):/)
-                        {
-                        $current_vg=$1;
-                        #print "\n$current_vg\n";
-                        }
-                if (/^pv_name=\/dev\/disk\/(\w+\d+):/)
-                        {
-                        $current_pv=$1;
-                        #print "$current_pv\n";
-                        $pvtable{$current_pv} = $current_vg;
-                        }
-                }
-        return $pvtable;
-        }
+	open(VGDISPLAYOUT,"$vg_cmd $ignore_error_output |") || die;
+	$_ = <VGDISPLAYOUT>;
+	while($_ = <VGDISPLAYOUT>)
+		{
+		if (/^vg_name=\/dev\/(\w+\d+):/)
+			{
+			$current_vg=$1;
+			#print "\n$current_vg\n";
+			}
+		if (/^pv_name=\/dev\/disk\/(\w+\d+):/)
+			{
+			$current_pv=$1;
+			#print "$current_pv\n";
+			$pvtable{$current_pv} = $current_vg;
+			}
+		}
+	return $pvtable;
+	}
 
 sub getiostat_linux
-        {
-        my $disk_name;
-        my $trans;
-        my ($iostat_cmd) = @_;
+	{
+	my $disk_name;
+	my $trans;
+	my ($iostat_cmd) = @_;
 
-        open(IOSTATOUT,"$iostat_cmd $ignore_error_output |") || die;
-        $_ = <IOSTATOUT>;
-        while($_ = <IOSTATOUT>)
-                {
-                #First output is stats average, which is useless for us, so we ignore the first time
-                #DEBUG
-                #print $_;
-                if (/Device:/)
-                        {
-                        $count += 1;
-                        }
-                if (( $count == 2 ) && (/([\w\d-]+)\s+([\d\.,]+)\s+([\d\.,]+)\s+([\d\.,]+)\s+([\d]+)\s+([\d]+)/))
-                        {
-                        #For Linux, iostat returns 6 columns as follow
-                        #device  tps  KB_read/s  KB_wrtn/s  KB_read  KB_wrtn
-                        $disk_name = $1;
-                        $disk_rate = ($3 + $4) * 1024;
-                        @iostatoutput = (@iostatoutput,"$disk_name;$disk_rate");
-                        }
-                }
-        close(IOSTATOUT);
-        return @iostatoutput;
-        }
+	open(IOSTATOUT,"$iostat_cmd $ignore_error_output |") || die;
+	$_ = <IOSTATOUT>;
+	while($_ = <IOSTATOUT>)
+		{
+		#First output is stats average, which is useless for us, so we ignore the first time
+		#DEBUG
+		#print $_;
+		if (/Device:/)
+			{
+			$count += 1;
+			}
+		if (( $count == 2 ) && (/([\w\d-]+)\s+([\d\.,]+)\s+([\d\.,]+)\s+([\d\.,]+)\s+([\d]+)\s+([\d]+)/))
+			{
+			#For Linux, iostat returns 6 columns as follow
+			#device  tps  KB_read/s  KB_wrtn/s  KB_read  KB_wrtn
+			$disk_name = $1;
+			$disk_rate = ($3 + $4) * 1024;
+			@iostatoutput = (@iostatoutput,"$disk_name;$disk_rate");
+			}
+		}
+	close(IOSTATOUT);
+	return @iostatoutput;
+	}
 
 sub getiostat_hpux
-        {
-        my $disk_name;
-        my $disk_rate;
-        my ($iostat_cmd) = @_;
+	{
+	my $disk_name;
+	my $disk_rate;
+	my ($iostat_cmd) = @_;
 
-        open(IOSTATOUT,"$iostat_cmd $ignore_error_output |") || die;
-        $_ = <IOSTATOUT>;
-        while($_ = <IOSTATOUT>)
-                {
-                #First output is stats average, which is useless for us, so we ignore the first time
-                #DEBUG
-                #print $_;
-                if (/^\s$/)
-                        {
-                        $count += 1;
-                        }
-                #if (( $count == 2 ) && (/^\s*(disk\d+|c\d+t\d+d\d+)\s+(\d+)\s+([\d+\.])\s+([\d+\.])/)) #YUNOWORK!!!
-                if (( $count == 2 ) && (/^\s*(disk\d+|c\d+t\d+d\d+)\s+(\d+)/))
-                        {
-                        #For HPUX, iostat returns 4 columns as follow
-                        #device  KB/s  NumSeek/s  ms/Seek(avg)
-                        $disk_name = $1;
-                        $disk_rate = $2 * 1024;
-                        @iostatoutput = (@iostatoutput,"$disk_name;$disk_rate");
-                        }
-                }
-        close(IOSTATOUT);
-        return @iostatoutput;
-        }
+	open(IOSTATOUT,"$iostat_cmd $ignore_error_output |") || die;
+	$_ = <IOSTATOUT>;
+	while($_ = <IOSTATOUT>)
+		{
+		#First output is stats average, which is useless for us, so we ignore the first time
+		#DEBUG
+		#print $_;
+		if (/^\s$/)
+			{
+			$count += 1;
+			}
+		#if (( $count == 2 ) && (/^\s*(disk\d+|c\d+t\d+d\d+)\s+(\d+)\s+([\d+\.])\s+([\d+\.])/)) #YUNOWORK!!!
+		if (( $count == 2 ) && (/^\s*(disk\d+|c\d+t\d+d\d+)\s+(\d+)/))
+			{
+			#For HPUX, iostat returns 4 columns as follow
+			#device  KB/s  NumSeek/s  ms/Seek(avg)
+			$disk_name = $1;
+			$disk_rate = $2 * 1024;
+			@iostatoutput = (@iostatoutput,"$disk_name;$disk_rate");
+			}
+		}
+	close(IOSTATOUT);
+	return @iostatoutput;
+	}
 
 sub getdata
-       {
-        my $vg_cmd;
-        my $iostat_cmd;
-        my @iostatoutput = ();
-        my @new_iostatoutput = ();
-        my $pvtable = {};
-        my $count = 0;
+	{
+	my $vg_cmd;
+	my $iostat_cmd;
+	my @iostatoutput = ();
+	my @new_iostatoutput = ();
+	my $pvtable = {};
+	my $count = 0;
 
-        my $disk;
-        my $disk_name;
-        my $disk_rate;
-        my $vg_name;
+	my $disk;
+	my $disk_name;
+	my $disk_rate;
+	my $vg_name;
 
-        if ($os eq "HP-UX")
-                {
-                #Getting VG/PV information
-                $pvtable = getpvpervg_hpux("vgdisplay -F -v | grep -v lv_name | grep -v pvg_name | grep -v deactivated");
+	if ($os eq "HP-UX")
+		{
+		#Getting VG/PV information
+		$pvtable = getpvpervg_hpux("vgdisplay -F -v | grep -v lv_name | grep -v pvg_name | grep -v deactivated");
 
-                #Getting iostat
-                @iostatoutput = getiostat_hpux("/usr/bin/iostat 1 2");
-                }
-        elsif ($os eq "Linux")
-                {
-                #Getting VG/PV information
-                @pvtable = getpvpervg_linux("pvs | grep -v PV");
+		#Getting iostat
+		@iostatoutput = getiostat_hpux("/usr/bin/iostat 1 2");
+		}
+	elsif ($os eq "Linux")
+		{
+		#Getting VG/PV information
+		@pvtable = getpvpervg_linux("pvs");
 
-                #Getting iostat
-                @iostatoutput = getiostat_linux("/usr/bin/iostat -d -k 1 2");
-                }
-        else
-                {
-                print "CRITICAL : OS $os not yet supported";
-                exit $ERRORS{"CRITICAL"};
-                }
+		#Getting iostat
+		@iostatoutput = getiostat_linux("/usr/bin/iostat -d -k 1 2");
+		}
+	else
+		{
+		print "CRITICAL : OS $os not yet supported";
+		exit $ERRORS{"CRITICAL"};
+		}
 
-        if (@iostatoutput)
-                {
-                #All disks are either tagged with their VG name or NOVG
-                foreach $disk (@iostatoutput)
-                        {
-                        ($disk_name,$disk_rate)=split(";", $disk);
-                        $vg_name = $pvtable{$disk_name} || "NOVG";
-                        #DEBUG
-                        #print $disk_name." ".$disk_rate." ".$vg_name."\n";
-                        @new_iostatoutput = (@new_iostatoutput, "$disk;$vg_name");
-                        }
-
-                return iostat_warnings(@new_iostatoutput);
-                }
-        else
-                {
-                print "UNKNOWN : No disk found, is iostat working?\n";
-                exit $ERRORS{"UNKNOW"};
-                }
-        }
+	if (@iostatoutput)
+		{
+		#All disks are either tagged with their VG name or NOVG	
+		foreach $disk (@iostatoutput)
+			{
+			($disk_name,$disk_rate)=split(";", $disk);
+			$vg_name = $pvtable{$disk_name} || "NOVG";
+			#DEBUG
+			#print $disk_name." ".$disk_rate." ".$vg_name."\n";
+			@new_iostatoutput = (@new_iostatoutput, "$disk;$vg_name");
+			}
+	
+		return iostat_warnings(@new_iostatoutput);
+		}
+	else
+		{
+		print "UNKNOWN : No disk found, is iostat working?\n";
+		exit $ERRORS{"UNKNOW"};
+		}
+	}
